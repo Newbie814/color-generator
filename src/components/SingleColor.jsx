67a53faa0bfda691ccdedfb8352dc5ae5aae6561
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2023-04-01 19:21:51 -0400

    connor component is just so connor could play with code without messing up my proj

--- src/components/SingleColor.jsx
+++ src/components/SingleColor.jsx
@@ -1,7 +1,10 @@
 const SingleColor = ({ color, index }) => {
   const { hex, weight } = color;
   return (
-    <article className='color' style={{ background: `#${hex}` }}>
+    <article
+      className={index > 7 ? 'color color-light' : 'color'}
+      style={{ background: `#${hex}` }}
+    >
       <p className='percent-value'>{weight}%</p>
       <p className='color-value'>#{hex}</p>
     </article>
