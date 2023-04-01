8d0b1951d6bf4921cd8cf4bf42e756ca0b9a4cf5
Newbie814 <96838174+Newbie814@users.noreply.github.com>
2023-04-01 19:54:04 -0400

    setup success and error toasts and copy to clipboard functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 import { useState } from 'react';
 import { ToastContainer, toast } from 'react-toastify';
-
+import 'react-toastify/dist/ReactToastify.css';
 import Form from './components/Form';
 import ColorList from './components/ColorList';
 import Values from 'values.js';
 
 const App = () => {
-  const [colors, setColors] = useState(new Values('#504acc').all(10));
+  const [colors, setColors] = useState(new Values('#504acc').all(5));
 
   const addColor = (color) => {
-    console.log(color);
+    try {
+      const newColors = new Values(color).all(10);
+      setColors(newColors);
+    } catch (error) {
+      console.log(error.message);
+      toast.error(error.message);
+    }
   };
   // toast.error('Error message');
   // toast.success('Success message');
@@ -18,7 +24,18 @@ const App = () => {
     <main>
       <Form addColor={addColor} />
       <ColorList colors={colors} />
-      <ToastContainer position='top-center' />
+      <ToastContainer
+        position='top-center'
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme='light'
+      />
     </main>
   );
 };
--- src/components/SingleColor.jsx
+++ src/components/SingleColor.jsx
@@ -1,9 +1,26 @@
+import { toast } from 'react-toastify';
+
 const SingleColor = ({ color, index }) => {
   const { hex, weight } = color;
+
+  const saveToClipboard = async () => {
+    if (navigator.clipboard) {
+      try {
+        await navigator.clipboard.writeText(`#${hex}`);
+        toast.success('Copied to clipboard');
+      } catch (error) {
+        console.log(error.message);
+        toast.error(error.message);
+      }
+    } else {
+      toast.error('Clipboard access not available');
+    }
+  };
   return (
     <article
-      className={index > 7 ? 'color color-light' : 'color'}
+      className={index > 14 ? 'color color-light' : 'color'}
       style={{ background: `#${hex}` }}
+      onClick={saveToClipboard}
     >
       <p className='percent-value'>{weight}%</p>
       <p className='color-value'>#{hex}</p>
